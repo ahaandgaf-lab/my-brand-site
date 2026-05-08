426eb47a31bacfd2a67e6e0dbb675601a5a56d51
ahaandgaf-lab <ahaandgaf@gmail.com>
2026-05-08 16:25:07 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+  cacheComponents: true,
 }
-module.exports = nextConfig;
+ 
+export default nextConfig
