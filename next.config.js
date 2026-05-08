174a90ee3b364ecaa594a4456b971fd6a3517faf
ahaandgaf-lab <ahaandgaf@gmail.com>
2026-05-08 16:27:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next'
+// @ts-check
  
-const nextConfig: NextConfig = {
-  cacheComponents: true,
+export default (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+  }
+  return nextConfig
 }
- 
-export default nextConfig
